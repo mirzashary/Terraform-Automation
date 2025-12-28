885ba7e2ab5230ae97c801c952b67c2d80d928df
mirzashary <86553601+mirzashary@users.noreply.github.com>
2025-12-29 00:40:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-app-project-terraform-state"
-    key = "main"
+    key = "ekss/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table-1"
     encrypt = "true"
